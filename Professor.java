fbe319fd0c7657e670a97c3bb473b1d18e613405
Deepika P <120781809+deepuzz11@users.noreply.github.com>
2024-06-25 14:57:35 +0530

    Create Professor.java

---
+++ Professor.java
@@ -0,0 +1,26 @@
+package Employee;
+
+public class Professor extends Employee {
+    double bPay;
+    String des;
+
+    public Professor(String name, String id, String address, String mail, String mobile,
+                     String designation, double basicPay) {
+        super(name, id, address, mail, mobile);
+        des = designation;
+        bPay = basicPay;
+    }
+
+    public void paySlip() {
+        float da = (float) (bPay * 0.97);
+        float hra = (float) (bPay * 0.10);
+        double grossSalary = bPay + da + hra;
+        float pf = (float) (bPay * 0.12);
+        double scf = bPay * 0.001;
+        double netSalary = grossSalary - pf - scf;
+        System.out.println("Designation: " + des);
+        System.out.println("Basic_Pay: " + bPay);
+        System.out.println("Gross Salary : " + grossSalary);
+        System.out.println("Net Salary : " + netSalary);
+    }
+}
